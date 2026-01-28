b2f4feba8989d4e7897c1607fab13f1800897b4a
Gavin Shaner <gshaner15@huskers.unl.edu>
2026-01-28 14:18:38 -0600

    Creating tic-tac-toe

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,41 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import TestMessage from "./components/TestMessage";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  
+  return (
+    <button 
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <TestMessage />
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </>
   );
 }
