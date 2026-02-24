9ae4efccbdc5171adbdb7e8b708bf43f00f989d7
JacksonDathe <jdathe2@huskers.unl.edu>
2026-02-24 14:40:58 -0600

    Models Update
    Have built out the beginnings of turn manager and game manager. Also got rid of the deck class, and updated colorCard to function without the deck.

--- server/src/Models/ColorCard.ts
+++ server/src/Models/ColorCard.ts
@@ -13,6 +13,16 @@ class ColorCard {
     ];
   }
 
+  /**
+   * Acts as a replacement for the Deck class.
+   * Generates a brand new random ColorCard on the fly.
+   */
+  public static drawRandomCard(): ColorCard {
+    // Generate a random ID (or you could use a timestamp/UUID in a full app)
+    const randomID = Math.floor(Math.random() * 10000); 
+    return new ColorCard(randomID);
+  }
+
   public getOption(index: number): ColorOption | undefined {
     return this.options[index];
   }
