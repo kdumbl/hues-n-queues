99822f6564d0180009a58841922b4760144b8c4f
kdumbl <kdumblauskas@gmail.com>
2026-02-26 19:07:13 -0600

    Created mongoose schemas

---
+++ server/src/schemas/game.schema.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+/*
+Basicallly a clone of the domain classes but in a mongodb format.
+Will be stored in mongo and can be used to recreate the game.
+*/
+
+const colorOptionSchema = new Schema({
+  optionID: Number,
+  hexCode: String,
+  gridCoordinates: String,
+});
+
+const colorCardSchema = new Schema({
+  cardID: Number,
+  options: [colorOptionSchema],
+});
+
+const playerSchema = new Schema({
+  playerName: String,
+  score: Number,
+  isClueGiver: Boolean,
+  _piecesRemaining: Number, //Private variable in player class?
+});
+
+const boardSchema = new Schema({
+  grid: Map, //Do we really need this if grid is the same everytime?
+  occupiedSpaces: [String],
+});
+
+const turnSchema = new Schema({
+  clueGiver: playerSchema,
+  activeCard: colorCardSchema,
+  targetOption: colorOptionSchema,
+  currentPhase: {
+    type: String,
+    enum: ["CLUE_ONE", "GUESS_ONE", "CLUE_TWO", "GUESS_TWO", "SCORING"],
+  },
+});
+
+const gameSchema = new Schema({
+  players: [playerSchema],
+  board: boardSchema,
+  currentTurnManager: turnSchema,
+  gameState: {
+    type: String,
+    enum: ["SETUP", "ACTIVE", "END"],
+  },
+});
