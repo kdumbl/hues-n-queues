264640b4ca8ead1848d95da649033b61bad1067e
Gavin Shaner <gshaner15@huskers.unl.edu>
2026-03-04 21:51:42 -0600

    Fixing changes, merging frontend parts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,12 @@
 import "./App.css";
 
 import GameView from "./GameView";
-
-function App() {
-  return <GameView />;
 import HCBoard from "./BoardScreen.tsx";
-import "./App.css";
 
 function App() {
   return (
     <>
-      <HCBoard />
+      <GameView />
     </>
   );
 }
