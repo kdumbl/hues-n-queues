35be382dd3b7fe4b0aa5fc9ce1e6fe70df63c081
Gavin Shaner <gshaner15@huskers.unl.edu>
2026-03-07 11:34:10 -0600

    Removed copies of GameView to only have GameScreen

--- client/api/socket.tsx
+++ client/api/socket.tsx
@@ -1,5 +1,4 @@
 import { io } from 'socket.io-client';
-import '../package.json';
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:5001';
