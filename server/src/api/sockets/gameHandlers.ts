40e44e6397a0d4234629c9d2c965f86809277454
Gavin Shaner <gshaner15@huskers.unl.edu>
2026-04-02 01:18:14 -0500

    Added some turn functionality
    -Dynamically creates multiple perspectives of master game state for each client
    -Allows clue givers to draw cards from 4 random colors and give clues
    -Allows piece placers to receive clues and place pieces
    -Changes permissions based on whose turn it is automatically
    -General polish and additional socket implementation

--- server/src/api/sockets/gameHandlers.ts
+++ server/src/api/sockets/gameHandlers.ts
@@ -1,19 +1,104 @@
 import { Server, Socket } from "socket.io";
 import { gameController } from "../controllers/gameController";
+import { Player, GameState } from "../types";
+
+const gavin: Player = {
+    name: "TheGooseMafia",
+    socketId: "45",
+    pieceColor: "RED",
+    profileURL: "rgebrgbergbj",
+    isClueGiver: true,
+    yourTurn: true,
+    score: 50,
+    clue: "",
+    piece: null,
+    secondPiece: null,
+  };
+
+const ruby: Player = {
+    name: "GutsMan",
+    socketId: "46",
+    pieceColor: "YELLOW",
+    profileURL: "rgebrgbergbj",
+    isClueGiver: false,
+    yourTurn: false,
+    score: 27,
+    clue: "",
+    piece: null,
+    secondPiece: null,
+  };
+
+const jackson: Player = {
+    name: "dather9",
+    socketId: "47",
+    pieceColor: "GREEN",
+    profileURL: "rgebrgbergbj",
+    isClueGiver: false,
+    yourTurn: false,
+    score: 9,
+    clue: "",
+    piece: null,
+    secondPiece: null,
+  };
+
+const kurt: Player = {
+    name: "DumblDum",
+    socketId: "48",
+    pieceColor: "BLUE",
+    profileURL: "rgebrgbergbj",
+    isClueGiver: false,
+    yourTurn: false,
+    score: 67,
+    clue: "",
+    piece: null,
+    secondPiece: null,
+  };
+
+let masterGameState: GameState = {
+  players: [gavin, ruby, jackson, kurt]
+};
 
 let usersConnected = 0;
-let master_images = Array(480).fill(null);
+let piecesPlaced = 0;
 
 //this is where all the emit and on functionality for the live game will live
-export function registerGameHandlers(io: Server, socket: Socket) {
+export function registerGameHandlers(io: Server, socket: Socket){
 
-  socket.on('player_submitted', (i, last_placed, piece) => {
-    if (last_placed != null){
-      master_images[last_placed] = null;
+  socket.once("new user", () => {
+    usersConnected += 1;
+    console.log("Learned that a new user has appeared, assigned them the number " + (usersConnected - 1));
+    io.emit("new user accepted", usersConnected - 1);
+  })
+
+  socket.on("guess_submitted", (lastPlayed, connectionNumber) => {
+    let x = lastPlayed % 30;
+    let y = Math.floor(lastPlayed / 30);
+    if (masterGameState.players[connectionNumber].piece == null){
+      masterGameState.players[connectionNumber].piece = {
+        x: x,
+        y: y
+      };
+    } else {
+      masterGameState.players[connectionNumber].secondPiece = {
+        x: x,
+        y: y
+      };
     }
-    master_images[i] = piece;
-    io.emit('piece_placed2', master_images);
-  });
+    masterGameState.players[connectionNumber].yourTurn = false;
+    masterGameState.players[(connectionNumber + 1) % 4].yourTurn = true;
+    piecesPlaced += 1;
+    io.emit("gameState updated", masterGameState);
+  })
+
+  socket.on("clue_submitted", (selectedColorIndex, cueText, connectionNumber) => {
+    console.log("The color they chose has index " + selectedColorIndex);
+    for (let i = 0; i < 4; i++){
+      masterGameState.players[i].clue = cueText;
+    }
+    masterGameState.players[connectionNumber].yourTurn = false;
+    masterGameState.players[(connectionNumber + 1) % 4].yourTurn = true;
+    io.emit("gameState updated", masterGameState);
+  })
 
   //receive info from a client
   socket.on("game_update", async (data: any) => {
